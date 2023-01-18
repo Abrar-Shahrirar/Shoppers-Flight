f60a9c8b563bdb6e2f567546c7600e8102fce4d8
Abrar Shahriar <94299568+Abrar-Shahrirar@users.noreply.github.com>
2023-01-18 21:04:02 +0600

    adding

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 from pathlib import Path
 import os
+import django
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -80,7 +81,7 @@ DATABASES = {
         'NAME': 'shoppers_flight',
         'USER': 'root',
         'PASSWORD':'',
-        'HOST':'localhost',
+        'HOST':'127.0.0.1',
         'PORT':'3306',
     }
 }
